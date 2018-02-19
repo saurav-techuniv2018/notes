bd4d1b17c0b720778d6c41e3c3138698b56d9aff
Saurav Sahu <sauravsahu.12@live.com>
2018-02-19 21:06:20 +0530

    Add PUT /notes route to replace notes in the database.
    Add PUT /notes route to replace notes in the database.

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,3 +1,5 @@
+const joi = require('joi');
+
 const models = require('../../../models');
 
 module.exports = [
@@ -24,6 +26,53 @@ module.exports = [
           response({
             statusCode: 500,
             error: 'Could not retrieve list of notes.',
+          });
+        });
+    },
+  },
+  {
+    path: '/notes',
+    method: 'PUT',
+    config: {
+      description: 'Replace the list of notes provided in the payload with the ones in the database',
+      tags: ['api'],
+      validate: {
+        payload: {
+          notes: joi.array().items(joi.object().keys({
+            id: joi.number().integer().min(0),
+            title: joi.string().required(),
+            note: joi.string().required(),
+          })),
+        },
+      },
+    },
+    handler: (request, response) => {
+      models
+        .notes
+        .destroy({ truncate: true })
+        .then(() => {
+          const notesWithIdProperty = request.payload.notes.map(note => ({
+            noteId: note.id,
+            title: note.title,
+            note: note.note,
+          }));
+
+          return models.notes.bulkCreate(notesWithIdProperty);
+        })
+        .then((newNotes) => {
+          response({
+            data: newNotes.map(note => ({
+              id: note.noteId,
+              title: note.title,
+              note: note.note,
+            })),
+            statusCode: 200,
+          });
+        })
+        .catch(() => {
+          response({
+            statusCode: 500,
+            error: 'Could not update notes in the database.',
           });
         });
     },
